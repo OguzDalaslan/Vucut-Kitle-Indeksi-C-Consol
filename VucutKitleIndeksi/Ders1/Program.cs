badbb53e02d03d3b0b0de249dd3e8cb5a5ec840e
OguzDalaslan <104154904+OguzDalaslan@users.noreply.github.com>
2022-04-21 18:29:16 +0300

    VucutKitleIndeksi

---
+++ VucutKitleIndeksi/Ders1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double boy, kilo, bdki;
+            Console.WriteLine("Boyunuzu Metre Cinsinden Giriniz:");
+            boy =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kilonuzu Kg olarak Giriniz");
+            kilo = Convert.ToDouble(Console.ReadLine());
+            bdki = kilo / (boy * boy);
+            Console.WriteLine("Beden Kitleniz:{0}", bdki);
+            Console.ReadKey();
+
+        }
+    }
+}
